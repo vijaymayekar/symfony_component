61eb6e55c1c9950d203e590c70c8c389a8dbe237
Vijay Mayekar <vijay.mayekar@tcs.com>
2017-04-17 14:17:55 +0530

    Add http kernel

---
+++ src/app.php
@@ -0,0 +1,39 @@
+<?php
+ 
+use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($year = null) {
+  if (null === $year) {
+    $year = date('Y');
+  }
+
+  return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
+
+class LeapYearController {
+  function indexAction($request) {
+    if (is_leap_year($request->attributes->get('year'))) {
+      return new Response('Yep, this is a leap year!');
+    }
+    return new Response('Nope, this is not a leap year.');
+  }
+}
+
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+  'year' => null,
+  '_controller' => array(new LeapYearController(), 'indexAction')
+)));
+
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+  'name' => 'World',
+  '_controller' => function ($request) {
+    return render_template($request);
+  }
+)));
+$routes->add('bye', new Routing\Route('/bye', array('_controller' => 'render_template')));
+
+return $routes;
