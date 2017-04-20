0da0222db6399d639bd1d65bb5cf7ce6bb66a81b
vijay mayekar <vijay.mayekar@tcs.com>
2017-04-20 15:51:47 +0530

    Final output of symfony

--- src/Calendar/Controller/LeapYearController.php
+++ src/Calendar/Controller/LeapYearController.php
@@ -12,13 +12,9 @@ class LeapYearController
   {
     $leapyear = new LeapYear();
     if ($leapyear->isLeapYear($year)) {
-      $response = new Response('Yep, this is a leap year!' . rand());
-    } else {
-      $response = new Response('Nope, this is not a leap year.' . rand());
+      return 'Yep, this is a leap year! ';
     }
 
-    $response->setTtl(10);
-
-    return $response;
+    return 'Nope, this is not a leap year.';
   }
 }
